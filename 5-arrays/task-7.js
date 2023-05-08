c1d231660264ed463b16427d50997dc4ca4c9434
Maksym Baboshko <maksym.baboshko@outlook.com>
2023-05-08 23:54:00 +0200

    Completed the 7th task

---
+++ 5-arrays/task-7.js
@@ -0,0 +1,37 @@
+/* 
+  Дан список задач
+  const tasks = ['Task 1'];
+
+  Сделать функции:
+  - Добавление задачи в конец
+  - Удаление задачи по названию
+  - Перенос задачи в начало списка по названию
+
+  Во всех случаях меняем (мутируем) исходный массив
+*/
+
+const tasks = ['Task 1'];
+
+const addTask = taskName => {
+  tasks.push(taskName);
+};
+
+const deleteTaskByTaskName = taskName => {
+  const taskIdx = tasks.indexOf(taskName);
+
+  if (taskIdx >= 0) {
+    return tasks.splice(taskIdx, 1).at(0);
+  }
+};
+
+const moveTaskToBeginning = taskName => {
+  tasks.unshift(deleteTaskByTaskName(taskName));
+};
+
+addTask('Task 2');
+addTask('Task 3');
+addTask('Task 4');
+deleteTaskByTaskName('Task 2');
+moveTaskToBeginning('Task 4');
+
+console.log(tasks);
