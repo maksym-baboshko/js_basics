262559d54c8ae73d93a1b9c53f382ba77eef834d
Maksym Baboshko <maksym.baboshko@outlook.com>
2023-05-19 08:19:33 +0200

    Made an example with scope & recursion

---
+++ 11-scope-and-this/1.js
@@ -0,0 +1,19 @@
+// Scope & Recursion
+
+const sum = 1;
+console.log('start');
+console.log(sum);
+
+function add5(n) {
+  n += 5;
+
+  if (n > 100) {
+    return n;
+  }
+
+  console.log(n);
+  return add5(n);
+}
+
+add5(sum);
+console.log('end');
